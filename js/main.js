34c58332a084f5363b062d5485218276ae9211e3
Yurii Khodzitskyi <yurochku@gmail.com>
2019-01-23 17:58:02 +0200

    hw-13 code fix, ready for adding modal

--- js/main.js
+++ js/main.js
@@ -2,77 +2,77 @@
     $(document).ready(function () {
         let mainArray = [];
         let secondArr = [];
+        let lastLoadedIdFromServer = 0;
         let parseLocal = JSON.parse(localStorage.getItem('localStorageArray'));
-            // {
-            //     "id": 0,
-            //     "title": 'text1',
-            //     "completed": false
-            // }
         if (parseLocal != null) {
             mainArray = parseLocal;
         }
-        let todoList = document.getElementById('todoList');
+        //let todoList = $('#todoList');
         renderArray(todoList);
         $.getJSON('https://jsonplaceholder.typicode.com/todos', function(data) {
             secondArr = data;
         });
         //add element button
-        $('#addButton').on('click',function getInputText() {
-            let text = document.getElementById('inputText').value;
-            if (text != '') {
-                mainArray.push({
-                    "id": mainArray.length,
-                    "title": text,
-                    "completed": false
-                });
-            }
-            document.getElementById('inputText').value = '';
-            if (mainArray != null) {
-                pushToLocalStorage();
-            }
-            renderArray(todoList);
-        });
-
         function renderArray(todoList) {
             todoList.innerHTML = '';
             for (i = 0; i < mainArray.length; i++) {
-                let li = document.createElement('li');
-                let todoStatus = document.createElement('input');
-                let rmBtn = document.createElement('button');
-                li.appendChild(todoStatus);
-                //check completed or not
+                let itemsCheckbox = '<input type="checkbox" id="todoListCheckboxItem' + i + '"" class="todoListCheckboxItem">';
+                let itemsText = '<span>' + mainArray[i].title + '</span>';
+                //let editBtn = '<button class="editBtn">'+ 'edit' +'</button>';   <a href="#ex1" rel="modal:open">Open Modal</a>
+                let editBtn = '<button class="editBtn">'+ 'edit' +'</button>';
+                let rmBtn = '<button class="rmBtn">' + '\u00D7' + '</button>';
+                //check for completed tasks
                 if (mainArray[i].completed === true) {
-                    li.setAttribute('style', 'text-decoration:line-through');
-                    todoStatus.setAttribute('checked', 'checked');
+                    let liItemPos = 'li' + i;
+                    let checkboxId = 'todoListCheckboxItem' + i;
+                    itemsText = '<span style="text-decoration: line-through">' + mainArray[i].title + '</span>';
+                    itemsCheckbox = '<input type="checkbox" id="todoListCheckboxItem' + i + '"" class="todoListCheckboxItem" checked="checked">';
                 }
-                todoStatus.setAttribute('type', 'checkbox');
-                todoStatus.setAttribute('class', 'todoListCheckboxItem');
-                li.appendChild(document.createTextNode(mainArray[i].title));
-                li.appendChild(rmBtn);
-                li.setAttribute('id','li' + i);
-                rmBtn.setAttribute('class', 'rmBtn');
-                rmBtn.appendChild(document.createTextNode('\u00D7'));
-                todoList.appendChild(li);
+                //create template for appending to the html
+                let template = '<li id="li' + i +'">'+itemsCheckbox + itemsText + editBtn + rmBtn + '</li>';
+                $('#todoList').append(template)
             }
+            //add new item from text input
+            $('#addButton').on('click',function getInputText() {
+                let text = $('#inputText').val();
+                if (text != '') {
+                    mainArray.push({
+                        "id": mainArray.length,
+                        "title": text,
+                        "completed": false
+                    });
+                }
+                $('#inputText').val('');
+                if (mainArray != null) {
+                    pushToLocalStorage();
+                }
+                renderArray(todoList);
+            });
             //delete element button
-            $('.rmBtn').click(function (e) {
+            $('.rmBtn').on('click',function (e) {
                 let li = e.target.parentElement.id;
                 let elem = parseInt(li.substr(2));
                 mainArray.splice(elem,1);
                 pushToLocalStorage();
                 renderArray(todoList);
             });
+            $('.editBtn').on('click',function (e) {
+                let li = e.target.parentElement.id;
+                let elem = parseInt(li.substr(2));
+                $('#inputText').val(mainArray[elem].title);
+                renderArray(todoList);
+            });
             //clear array and local storage
-            $('#clearArrayStorage').click(function () {
+            $('#clearArrayStorage').on('click',function () {
                 localStorage.clear();
                 $('#todoList').empty();
                 mainArray = [];
             });
             //get parent id and set parameter "completed"
-            $('.todoListCheckboxItem').click(function (e) {
+            $('.todoListCheckboxItem').on('click',function (e) {
                 let li = e.target.parentElement.id;
                 let elem = parseInt(li.substr(2));
-                if (e.target.checked == true) {
+                if (e.target.checked === true) {
                     mainArray[elem].completed = true;
                 }
                 else {
@@ -82,12 +82,17 @@
                 renderArray(todoList);
             });
         }
-        $('#loadMoreTodos').click(function () {
+        $('#loadMoreTodos').on('click',function () {
+            let loadFromServerLenght = lastLoadedIdFromServer + 10;
                 $.getJSON('https://jsonplaceholder.typicode.com/todos', function(data) {
                     secondArr = data;
                 });
-            for (let i = 0; i <= 9; i++) {
+            for (let i = lastLoadedIdFromServer; i < loadFromServerLenght; i++) {
                 mainArray.push(secondArr[i]);
+                lastLoadedIdFromServer = secondArr[i].id;
+                if (lastLoadedIdFromServer > secondArr.length - 1) {
+                    lastLoadedIdFromServer = 0;
+                }
                 pushToLocalStorage();
                 renderArray(todoList);
             }
